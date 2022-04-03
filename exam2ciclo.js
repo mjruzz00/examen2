8ea453135efa193906eb3c833f25ca794301f7d4
mjruzz00 <99859690+mjruzz00@users.noreply.github.com>
2022-04-03 17:29:32 -0500

    Add files via upload

--- exam2ciclo.js
+++ exam2ciclo.js
@@ -1,40 +1,38 @@
-function vocales(){
-	voc="AEIOUÁÉÍÓÚÜ";
-	letra="?";
-	for(i=1;i<ap.length;i++){
-		letra=ap.charAt(i);
-		if (voc.includes(letra)) return(letra);
-	}
-	return(letra);
-}
-
 function ejecuta(){
 	nom=document.TEC.nom.value;
-	document.TEC.nom.style.background="blue";
-	if (nom.length==0) {
+    document.TEC.nom.style.background="blue";
+    if (nom.length==0){
 		document.TEC.nom.style.background="red";
-		alert("Error, se debe indicar el Nombre...");
+		alert("Error, se debe indicar el Nombre ...");
 	}
 	ap=document.TEC.ap.value;
-	document.TEC.ap.style.background="blue";
-	if (ap.length==0) {
+    document.TEC.ap.style.background="blue";
+    if (ap.length==0){
 		document.TEC.ap.style.background="red";
-		alert("Error, se debe indicar el Apellido Paterno...");
+		alert("Error, se debe indicar el Apellido paterno ...");
 	}
 	am=document.TEC.am.value;
-	document.TEC.am.style.background="blue";
-	if (am.length==0) {
+    document.TEC.am.style.background="blue";
+    if (am.length==0){
 		document.TEC.am.style.background="red";
-		alert("Error, se debe indicar el Apellido Materno...");
+		alert("Error, se debe indicar el Apellido materno ...");
 	}
 	fec=document.TEC.fec.value;
-	document.TEC.am.style.background="blue";
-	if (fec.length==0) {
-		document.TEC.fec.style.background="red";
-		alert("Error, se debe indicar la fecha...");
-	}
-	salida=document.TEC.salida.value;
-	document.TEC.salida.style.background="blue";
-	if (salida.length==0) {
-	correo=ap.chart(0)+nom+am.chart(0)+"@empresa.com.mx"
+	nom=nom.toUpperCase();
+	ap=ap.toUpperCase();
+	am=am.toUpperCase();
+	anio=fec.substr(2,2);
+	mes=fec.substr(5,2);
+	dia=fec.substr(8,2);
+	correo=nom.charAt(0).toLowerCase()+ap.toLowerCase()+am.charAt(0).toLowerCase()+"@empresa.com.mx";
+    correo=correo.replaceAll("ñ","n");
+    correo=correo.replaceAll(" ","_");
+    correo=correo.replaceAll("á","a");
+    correo=correo.replaceAll("é","e");
+    correo=correo.replaceAll("í","i");
+    correo=correo.replaceAll("ó","o");
+    correo=correo.replaceAll("ú","u");
+    document.TEC.salida.value=correo;
+    contra=dia+mes+anio;
+    document.TEC.contra.value=contra;
 }
